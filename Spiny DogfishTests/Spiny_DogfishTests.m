9f094283dffb23dfa8d55a318d341ea918ddb7c5
Max Korenkov <mkorenkov@gmail.com>
2011-12-01 03:41:36 +0200

    one more test case

--- Spiny DogfishTests/Spiny_DogfishTests.m
+++ Spiny DogfishTests/Spiny_DogfishTests.m
@@ -75,4 +75,15 @@
     STAssertTrue([[processor getTranslation] isEqualToString:@"1. сущ. 1) проверка , испытание ; тест 2) а) проверочная , контрольная работа ; тест б) психол.  тест 3) мерило ; критерий 4) мед. ; хим.  исследование , анализ ; проверка 5) хим.  реактив 6) пробирная чашка для определения пробы  ( драгоценного металла ) 2. гл. 1) а) подвергать испытанию , проверке б) подвергаться испытанию , проходить тест в) амер.  показать в результате испытания , дать результат ; обнаруживать определённые свойства в результате испытаний 2) а) = test out тестировать ; проверять с помощью тестов б) экзаменовать 3) проверять , убеждаться 4) а) хим.  подвергать действию реактива ; брать пробу б) производить опыты в) определять пробу  ( драгоценного металла ) 3. прил. испытательный , пробный , контрольный , проверочный"], @"Not expected result: %@", [processor getTranslation]);
 }
 
+- (void)test_process_enru_random
+{
+    NSString *src = @"random LingvoUniversal (En-Ru) [ =\"/Handlers/TranscriptionHandler.ashx?Text='r%C3%A6nd%C9%99m\"] брит.                      амер.                      прил. сделанный или выбранный наугад ; случайный , произвольный Розгорнути статтю &#187; &#171; Згорнути статтю ";
+    Eng2RuPostProcessor *processor = [[Eng2RuPostProcessor alloc]init];
+    [processor process:[[NSMutableString alloc] initWithString:src]];
+    STAssertTrue([[processor getWord] isEqualToString:@"random"], @"Not expected result: %@", [processor getWord]);
+    STAssertTrue([[processor getDictionary] isEqualToString:@"LingvoUniversal (En-Ru)"], @"Not expected result: %@", [processor getDictionary]);
+    STAssertTrue([[processor getTranscriptionUrl] isEqualToString:@"/Handlers/TranscriptionHandler.ashx?Text='r%C3%A6nd%C9%99m"], @"Not expected result: %@", [processor getTranscriptionUrl]);
+    STAssertTrue([[processor getTranslation] isEqualToString:@"прил. сделанный или выбранный наугад ; случайный , произвольный"], @"Not expected result: %@", [processor getTranslation]);
+}
+
 @end
