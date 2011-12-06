f0f73ede14348176a0d48013bc6943fe6f4150ce
Max Korenkov <mkorenkov@gmail.com>
2011-12-06 04:59:41 +0200

    tests fixed

--- Spiny DogfishTests/Spiny_DogfishTests.m
+++ Spiny DogfishTests/Spiny_DogfishTests.m
@@ -109,7 +109,7 @@
     Eng2RuHTMLParser *parser = [[Eng2RuHTMLParser alloc] init];
     @try {
         NSMutableString *result = [parser parseHTMLString:content];
-        NSString *expected = @"test LingvoUniversal (En-Ru) [ =\"/Handlers/TranscriptionHandler.ashx?Text=test\"] брит.                      / амер.                      1. сущ. 1) проверка , испытание ; тест 2) а) проверочная , контрольная работа ; тест б) психол.  тест 3) мерило ; критерий 4) мед. ; хим.  исследование , анализ ; проверка 5) хим.  реактив 6) пробирная чашка для определения пробы  ( драгоценного металла ) 2. гл. 1) а) подвергать испытанию , проверке б) подвергаться испытанию , проходить тест в) амер.  показать в результате испытания , дать результат ; обнаруживать определённые свойства в результате испытаний 2) а) = test out тестировать ; проверять с помощью тестов б) экзаменовать 3) проверять , убеждаться 4) а) хим.  подвергать действию реактива ; брать пробу б) производить опыты в) определять пробу  ( драгоценного металла ) 3. прил. испытательный , пробный , контрольный , проверочный Розгорнути статтю &#187; &#171; Згорнути статтю ";
+        NSString *expected = @"прик о льный Universal (Ru-En) жарг. zany ";
         STAssertTrue( [result isEqualToString: expected], @"Not expected result: %@", result);
     } @catch (Eng2RuNotFoundException *e) {
         STFail(@"Parsing this word should not give errors.");
@@ -127,7 +127,7 @@
     Eng2RuHTMLParser *parser = [[Eng2RuHTMLParser alloc] init];
     @try {
         NSMutableString *result = [parser parseHTMLString:content];
-        NSString *expected = @"test LingvoUniversal (En-Ru) [ =\"/Handlers/TranscriptionHandler.ashx?Text=test\"] брит.                      / амер.                      1. сущ. 1) проверка , испытание ; тест 2) а) проверочная , контрольная работа ; тест б) психол.  тест 3) мерило ; критерий 4) мед. ; хим.  исследование , анализ ; проверка 5) хим.  реактив 6) пробирная чашка для определения пробы  ( драгоценного металла ) 2. гл. 1) а) подвергать испытанию , проверке б) подвергаться испытанию , проходить тест в) амер.  показать в результате испытания , дать результат ; обнаруживать определённые свойства в результате испытаний 2) а) = test out тестировать ; проверять с помощью тестов б) экзаменовать 3) проверять , убеждаться 4) а) хим.  подвергать действию реактива ; брать пробу б) производить опыты в) определять пробу  ( драгоценного металла ) 3. прил. испытательный , пробный , контрольный , проверочный Розгорнути статтю &#187; &#171; Згорнути статтю ";
+        NSString *expected = @"прилож е ние Universal (Ru-En)                     с. 1) ( прикладывание ) apposition , affixing 2) ( приложенные документы и т.п. ) enclosure , attachment 3) ( к журналу и т.п. ) appendix  ( pl  -ices [ =\"/Handlers/TranscriptionHandler.ashx?Text=-%C9%AAsi%CB%90z\"<spanclass=\"Italic\"], -ixes ) ; supplement ; ( к договору ) appendix , schedule 4) грам.  apposition 5) ( применение ) application 6) информ.  ( прикладная программа ) application ; ( прикладной модуль программы ) plug-in Розгорнути статтю &#187; &#171; Згорнути статтю ";
         STAssertTrue( [result isEqualToString: expected], @"Not expected result: %@", result);
     } @catch (Eng2RuNotFoundException *e) {
         STFail(@"Parsing this word should not give errors.");
