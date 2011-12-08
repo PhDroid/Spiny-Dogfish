1f6c2f0a2892f284936750741a3b06fc51c71111
Max Korenkov <mkorenkov@gmail.com>
2011-12-08 22:01:20 +0200

    stub

---
+++ Spiny DogfishTests/EnRuProcess_Tests.m
@@ -0,0 +1,27 @@
+//
+//  Created by lynx on 12/8/11.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "EnRuProcess_Tests.h"
+
+
+@implementation EnRuProcess_Tests
+
+-(void)setUp {
+    [super setUp];
+    // Set-up code here.
+}
+
+- (void)tearDown {
+    // Tear-down code here.
+    [super tearDown];
+}
+
+- (void)test_process_enru_hello {
+
+}
+
+@end
